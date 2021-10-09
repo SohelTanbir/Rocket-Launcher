54b6a74375def5f01b21a1ee9ab9138aee0b78bf
sohelTanbir <you@example.com>
2021-10-09 12:05:42 +0600

    simple footer section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <SearchBar/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () =>{
+    return(
+       <div className="footer container text-center">
+           <hr />
+            <p className="text-muted">All Right Reserved &copy; Assignment from Technext </p>
+       </div>
+    )
+}
+
+export default Footer;
